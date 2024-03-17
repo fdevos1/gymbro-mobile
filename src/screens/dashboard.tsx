10b119afd8f7004953acbda65a4bd610b69b59c3
fdevos1 <felipe.devosm@gmail.com>
2024-03-16 21:26:32 -0300

    started to implement animation o dash

--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -1,55 +1,85 @@
-import Icon from 'react-native-vector-icons/FontAwesome';
 import React from 'react';
-import {View, Text, TouchableHighlight} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+
+const {height: SCREEN_HEIGHT} = Dimensions.get('window');
+const MAX_TRANSLATE_Y = SCREEN_HEIGHT / 1.5;
 
 function Dashboard() {
-  return (
-    <View style={{padding: 16, backgroundColor: '#fafaf9', gap: 80}}>
-      <View style={{gap: 16}}>
-        <Text style={{fontSize: 20, fontWeight: '700', color: '#000'}}>
-          Inicio rápido
-        </Text>
+  const translateY = useSharedValue(0);
+  const reanimatedBottomStyle = useAnimatedStyle(e => {
+    return {
+      transform: [{translateY: translateY.value}],
+    };
+  });
 
-        <TouchableHighlight
-          style={{
-            backgroundColor: 'rgba(66,133,244, 0.75)',
-            borderRadius: 4,
-            padding: 4,
-            width: '100%',
-            alignItems: 'center',
-          }}>
-          <Text style={{color: 'white', fontWeight: '700', fontSize: 16}}>
-            Começar novo treino
-          </Text>
+  return (
+    <Animated.View style={[styles.container, reanimatedBottomStyle]}>
+      <View style={styles.header}>
+        <Text style={styles.quickStartText}>Inicio rápido</Text>
+
+        <TouchableHighlight style={styles.button}>
+          <Text style={styles.buttonText}>Começar novo treino</Text>
         </TouchableHighlight>
       </View>
 
-      <View style={{width: '100%', height: 200}}>
-        <View style={{alignItems: 'center'}}>
-          <TouchableHighlight
-            style={{
-              width: '80%',
-              backgroundColor: 'rgba(66,133,244, 0.75)',
-              alignItems: 'center',
-              borderRadius: 4,
-            }}>
-            <View
-              style={{
-                flexDirection: 'row',
-                padding: 4,
-                alignItems: 'center',
-                gap: 8,
-              }}>
-              <Text style={{fontSize: 16, fontWeight: '700', color: 'white'}}>
-                Criar modelo novo
-              </Text>
-              <Icon name="plus" color="#fff" size={16} />
-            </View>
-          </TouchableHighlight>
-        </View>
+      <View style={styles.body}>
+        <TouchableHighlight style={styles.button}>
+          <View>
+            <Text style={styles.buttonText}>Criar novo modelo</Text>
+          </View>
+        </TouchableHighlight>
       </View>
-    </View>
+    </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: SCREEN_HEIGHT,
+
+    padding: 16,
+    gap: 80,
+    position: 'absolute',
+
+    top: SCREEN_HEIGHT / 1.5,
+    zIndex: 12000,
+  },
+  header: {
+    gap: 16,
+  },
+  quickStartText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#000',
+  },
+  button: {
+    backgroundColor: 'rgba(66,133,244, 0.75)',
+    alignItems: 'center',
+    borderRadius: 4,
+    padding: 4,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  body: {
+    width: '100%',
+    height: 200,
+    paddingHorizontal: 16,
+  },
+});
 
 export default Dashboard;
